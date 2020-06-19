00ce20e150d2872606966bc4516628ea370abc17
yyb <986432727@qq.com>
2020-06-19 22:21:56 +0800

    P55

---
+++ src/main/java/com/yyb/lc/greed/P55.java
@@ -0,0 +1,22 @@
+package com.yyb.lc.greed;
+
+public class P55 {
+    public static void main(String[] args) {
+        P55 p = new P55();
+        p.canJump(new int[]{3, 2, 1, 0, 4});
+    }
+
+    private boolean canJump(int[] nums) {
+        int jumpMost = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (jumpMost >= i) {
+                jumpMost = Math.max(jumpMost, nums[i] + i);
+                if (jumpMost >= nums.length - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
